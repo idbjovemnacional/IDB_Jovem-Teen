e500291501b8deedff0a46d78e86c056baa1f0ec
Gabriel Monteiro <gabrielmonteiro1203@gmail.com>
2026-05-18 04:22:25 -0300

    feat: Config inicial

---
+++ src/constants/theme.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: "#FF6D2C",
+  secondary: "#FDF3EA",
+  darkProgram: "#93370D",
+  black: "#1E1E1E",
+  white: "#FFFFFF",
+};
+
+export const FONTS = {
+  primary: "Inter",
+  handwriting: "Covered By Your Grace",
+};
